74a2a9a421a2925c451fc8ee389111234ddef13d
lbeya <lbeya99@gmail.com>
2024-10-16 17:06:39 -0100

    Ajout des modifications dans pom.xml

---
+++ src/test/java/UserServiceImplTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.tpfoyer.entity.Etudiant;
+import tn.esprit.tpfoyer.repository.EtudiantRepository;
+import tn.esprit.tpfoyer.service.EtudiantServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+class UserServiceImplTest {
+    @Mock
+    EtudiantRepository userRepository;
+
+    @InjectMocks
+    EtudiantServiceImpl userService;
+
+    Etudiant user = new Etudiant();
+
+    List<Etudiant> listUsers = new ArrayList<>() {
+        {
+            add(new Etudiant());
+            add(new Etudiant());
+        }
+    };
+
+    @Test
+    @Order(1)
+    public void testRetrieveUser() {
+        Mockito.when(userRepository.findById(Mockito.anyLong())).
+                thenReturn(Optional.of(user));
+        Etudiant user1 = userService.retrieveEtudiant(2L);
+        Assertions.assertNotNull(user1);
+
+    }
+
+    @Test
+    public void testRetrieveAllUsers() {
+        Mockito.when(userRepository.findAll()).thenReturn(listUsers);
+        List<Etudiant> listU = userService.retrieveAllEtudiants();
+        Assertions.assertEquals(2, listU.size());
+    }
+}
+
